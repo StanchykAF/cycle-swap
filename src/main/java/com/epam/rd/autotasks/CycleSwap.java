08bbb653a178d57bdedacfbecc8ad7cec3d2d479
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-04-08 18:48:20 +0300

    Solved!

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -1,11 +1,22 @@
 package com.epam.rd.autotasks;
 
-class CycleSwap {
-    static void cycleSwap(int[] array) {
+import java.util.Arrays;
 
+class CycleSwap {
+
+    static void cycleSwap(int[] array) {
+        if (array.length != 0){
+            int[] buf = Arrays.copyOf(array, array.length);
+            System.arraycopy(buf, 0, array, 1, buf.length - 1);
+            array[0] = buf[buf.length - 1];
+        }
     }
 
     static void cycleSwap(int[] array, int shift) {
-
+        if ((array.length != 0) && (shift != array.length)) {
+            int[] buf = Arrays.copyOf(array, array.length);
+            System.arraycopy(buf, 0, array, shift, buf.length - shift);
+            System.arraycopy(buf, buf.length - shift, array, 0, shift);
+        }
     }
 }
